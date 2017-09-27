2def701fc946820e0c29f696cdf7a310f18e52cf
Shikhar <shikarvaish@gmail.com>
2017-09-27 23:17:35 +0530

    added hinderance objects in the code

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pygame
 import time
+import random
 # initiate the pygame module
 pygame.init()
 # making variables for display_width and display_height
 display_height = 600
 display_width = 800
-# we'll define colour using RGB methodology
+# we'll define color using RGB methodology
 black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
@@ -20,6 +21,14 @@ pygame.display.set_caption('A bit racey')
 clock = pygame.time.Clock()
 # loading image in a variable
 carImg = pygame.image.load('img/racecar.png')
+
+# defining a function make objects for avoiding things
+def things(thingx , thingy , thingw ,thingh, color):
+    pygame.draw.rect(gameDisplay , color, [thingx , thingy ,thingw , thingh])
+
+
+
+
 # creating a function to display car on the background at the position x and y from the origin
 def car(x,y):
     gameDisplay.blit(carImg,(x,y))
@@ -49,6 +58,13 @@ def game_loop():
     x = (display_width * 0.45)
     y = (display_height * 0.8)
     x_change = 0
+# defining the location of the objects
+    thing_startx = random.randrange(0 , display_width)
+    thing_starty = -600
+    thing_speed = 7
+    thing_width = 100
+    thing_height = 100
+
 
     # by default we'll start the game as not crashed
     gameExit = False
@@ -75,11 +91,20 @@ def game_loop():
         x+= x_change
 
         gameDisplay.fill(white) #this will display whole of the background as white
+        #things(thingx , thingy ,thingw , thingh, color)
+        things(thing_startx , thing_starty , thing_width , thing_height , black)
+
+        thing_starty += thing_speed
+
         car(x,y) # calling the function to car to display the car by giving them the parameter of x and y, which are the starting point of the car image
 
     #making boundaries to crash when the car hits the boundary
         if x > display_width - car_width or x < 0:
             crash()
+        if thing_starty > display_height:
+            thing_starty = 0 - thing_height
+            thing_startx = random.randrange( 0 , display_width)
+
 
         pygame.display.update()
         clock.tick(60) #the no here is fps
