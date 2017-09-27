bae5772f5dc06aaa696efa9b8b1c12825e673bff
Shikhar <shikarvaish@gmail.com>
2017-09-28 00:02:07 +0530

    added crossovers so as to track if we've hit the block or not

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def game_loop():
     x_change = 0
 # defining the location of the objects
     thing_startx = random.randrange(0 , display_width)
-    thing_starty = -600
+    thing_starty = 600
     thing_speed = 7
     thing_width = 100
     thing_height = 100
@@ -104,6 +104,13 @@ def game_loop():
         if thing_starty > display_height:
             thing_starty = 0 - thing_height
             thing_startx = random.randrange( 0 , display_width)
+
+        if y < thing_starty+thing_height:
+            print ('y crossover')
+
+            if x > thing_startx and x < thing_startx+thing_width or x+car_width > thing_startx and x+car_width < thing_startx+thing_width:
+                print (' x crossover')
+                crash()
 
 
         pygame.display.update()
